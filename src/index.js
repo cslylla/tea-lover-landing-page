a3edd6fea195b8dae67365c2d93c54767d7874ca
cslylla <81328857+cslylla@users.noreply.github.com>
2022-11-06 18:26:56 +0100

    Add dark mode and update link texts

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+//Change theme
+let body = document.querySelector("body");
+let themeButton = document.querySelector(".theme-button");
 
+function changeTheme() {
+  if (body.classList.contains("dark")) {
+    body.classList.remove("dark");
+    themeButton.innerHTML = "Dark mode";
+  } else {
+    body.classList.add("dark");
+    themeButton.innerHTML = "Light mode";
+  }
+}
+
+themeButton.addEventListener("click", changeTheme);
